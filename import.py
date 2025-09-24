f33d7182088813b4b4a0d758ab236a7798040b4c
Jakob Klotz <jakob.klotz@mci.edu>
2025-09-24 13:10:15 +0200

    chore: subfolders for data sets

--- import.py
+++ import.py
@@ -4,10 +4,10 @@ from src.processors.geosphere import GeoSphere
 
 def import_data():
     geosphere = GeoSphere(
-        file_path="data/raw/geosphere.gpkg",
-        metadata_file="data/raw/geosphere.meta.json",
+        file_path="data/raw/geosphere/geosphere.gpkg",
+        metadata_file="data/raw/geosphere/geosphere.meta.json",
     )
-    geosphere.run(file_dump="data/processed/geosphere.gpkg")
+    geosphere.run(file_dump="data/processed/geosphere/geosphere.gpkg")
 
 
 import_data()
