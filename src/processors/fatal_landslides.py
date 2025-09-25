d8a4e15bc73f0aa4c28f70e6b25afd64727a83cc
JakobKlotz <jakob.klotz@mci.edu>
2025-09-25 10:03:41 +0200

    feat: more error prone matching of types

--- src/processors/fatal_landslides.py
+++ src/processors/fatal_landslides.py
@@ -3,6 +3,8 @@ import warnings
 from pathlib import Path
 
 import geopandas as gpd
+import pandas as pd
+from shapely.geometry import Point
 
 from src.constants import AUSTRIA, TARGET_CRS
 
@@ -28,26 +30,58 @@ class GlobalFatalLandslides:
             + self.data["Report_1"]
             + "; Source: "
             + self.data["Source_1"]
-        )
+        ).str.replace("\n", " ")  # remove unnecessary newlines
         # Drop unused columns
-        self.data = self.data[["Date", "description", "geometry"]].reset_index(
-            drop=True
-        )
+        self.data = self.data[["Date", "description", "geometry"]]
+        self.data = self.data.sort_values("Date").reset_index(drop=True)
 
-        # Manually assign type (first and last one is a rockfall)
+    def clean(self):
+        """Clean the data."""
+        # Manually assign type (landslide, rockfall)
         warnings.warn(
-            message="Caution: Types are manually assigned to the data."
+            message="Caution: Types are assigned to the data."
             "If you have changed the source data of the global fatal landslide"
             " database check the results",
             stacklevel=2,
         )
-        # Default type
-        self.data["type"] = "landslide"
-        # Those two events were rockfalls, see the description
-        rockfall_dates = ["2005-08-23 00:00:00", "2008-03-01 00:00:00"]
-        self.data.loc[self.data["Date"].isin(rockfall_dates), "type"] = (
-            "rockfall"
+
+        # Remove Z coordinate from points
+        self.data["geometry"] = self.data["geometry"].force_2d()
+        self.data = self.data.to_crs(crs=TARGET_CRS)
+
+        # Combination of date & geometry is unique
+        # Those two events were rockfalls, see their description
+        rockfall_events = gpd.GeoDataFrame(
+            data={
+                "Date": pd.to_datetime(
+                    ["2005-08-23 00:00:00", "2008-03-01 00:00:00"]
+                ),
+                "geometry": [
+                    Point(651192.3868625985, 5212271.343543028),
+                    Point(807247.7813673844, 5256032.494610518),
+                ],
+                "type_override": ["rockfall", "rockfall"],
+            },
+            crs=self.data.crs,
         )
 
-    def convert_crs(self):
-        self.data = self.data.to_crs(crs=TARGET_CRS)
+        # Perform a spatial join (to account for slight floating point
+        # differences in the geometries)
+        self.data = gpd.sjoin_nearest(
+            self.data,
+            rockfall_events,
+            how="left",
+            max_distance=1,
+        )
+        print(self.data)
+        # A spatial match is only valid if the dates also match.
+        # Invalidate matches where the dates are different.
+        date_mismatch = self.data["Date_left"] != self.data["Date_right"]
+        self.data.loc[date_mismatch, "type_override"] = None
+
+        # All other events are landslides (the default)
+        self.data["type"] = self.data["type_override"].fillna("landslide")
+        # Drop helper columns from the join
+        self.data = self.data.drop(
+            columns=["type_override", "index_right", "Date_right"]
+        ).rename(columns={"Date_left": "Date"})
