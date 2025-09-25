af82fa7eb0cebc3d80167e1aa5b857298f15bc97
Jakob Klotz <jakob.klotz@mci.edu>
2025-09-25 17:52:18 +0200

    feat: import global fatal landslide database

--- import.py
+++ import.py
@@ -1,13 +1,34 @@
 # Dedicated import script
+from pathlib import Path
+
+from src.processors.fatal_landslides import GlobalFatalLandslides
 from src.processors.geosphere import GeoSphere
 
 
 def import_data():
+    in_base_path, out_base_path = Path("./data/raw"), Path("./data/processed")
+    # -------------------------------------------------------------------------
+    # GeoSphere
+    # -------------------------------------------------------------------------
     geosphere = GeoSphere(
-        file_path="data/raw/geosphere/geosphere.gpkg",
-        metadata_file="data/raw/geosphere/geosphere.meta.json",
+        file_path=in_base_path / "geosphere/geosphere.gpkg",
+        metadata_file=in_base_path / "geosphere/geosphere.meta.json",
     )
-    geosphere.run(file_dump="data/processed/geosphere/geosphere.gpkg")
+    geosphere.run(file_dump=out_base_path / "geosphere/geosphere.gpkg")
+
+    # -------------------------------------------------------------------------
+    # Global Fatal Landslides
+    # -------------------------------------------------------------------------
+    fatal_landslides = GlobalFatalLandslides(
+        file_path=in_base_path
+        / "global-fatal-landslides/global-fatal-landslides.gpkg",
+        metadata_file=in_base_path
+        / "global-fatal-landslides/global-fatal-landslides.meta.json",
+    )
+    fatal_landslides.run(
+        file_dump=out_base_path
+        / "global-fatal-landslides/global-fatal-landslides.gpkg"
+    )
 
 
 import_data()
