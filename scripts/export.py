727e17a8c68358953d33d9b4f15c6b135a4fecde
Jakob Klotz <jakob.klotz@mci.edu>
2025-10-15 13:49:37 +0200

    feat: helper script to export as GeoPackage

---
+++ scripts/export.py
@@ -0,0 +1,17 @@
+# Connect to the db and export the landslide_view as GeoPackage
+import geopandas as gpd
+from src.utils import create_db_session
+
+out_file = "landslides-db"
+
+Session = create_db_session()
+with Session() as session:
+    landslide_view = gpd.read_postgis(
+        "SELECT * FROM landslides_view", session.bind, geom_col="geom"
+    )
+    landslide_view.to_file(f"{out_file}.gpkg", driver="GPKG")
+    print("Exported!")
+
+    landslide_view.source_name.value_counts().to_csv(
+        f"{out_file}.csv", index=True
+    )
